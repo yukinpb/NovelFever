0e3a073d70928ee099bbf8bbd166331f87b51720
Hoang Pham <phh61002@gmail.com>
2024-07-06 09:11:02 +0700

    update build.gradle.kts

---
+++ app/src/main/java/com/example/novelfever/core/enums/BookStatus.kt
@@ -0,0 +1,10 @@
+package com.example.novelfever.core.enums
+
+enum class BookStatus {
+    COMPLETED,
+    ONGOING,
+    UPCOMING,
+    UNKNOWN,
+    ABANDONED,
+    PAUSED
+}
--- app/src/main/java/com/example/novelfever/core/model/Book.kt
+++ app/src/main/java/com/example/novelfever/core/model/Book.kt
@@ -1,9 +1,11 @@
 package com.example.novelfever.core.model
 
+import com.example.novelfever.core.enums.BookSource
+
 data class Book(
     val coverUrl: String? = null,
-    val title: String? = null,
-    val url: String? = null,
+    val title: String,
+    val url: String,
     val latestChapter: String? = null,
-    val bookSource: BookSource? = null
+    val bookSource: BookSource
 )
