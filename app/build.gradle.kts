0cbf08b2f89e69fe7fc5d90880d52160abb0acda
hp6thoct <148302503+hp6thoct@users.noreply.github.com>
2024-07-10 11:05:00 +0700

    add list view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,6 +97,15 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
 
     implementation(libs.accompanist.pager)
+
+    //window manager
+    implementation(libs.androidx.window)
+
+    //coil
+    implementation(libs.coil)
+
+    //Coil compose
+    implementation(libs.coil.compose)
 }
 // Allow references to generated code
 kapt {
